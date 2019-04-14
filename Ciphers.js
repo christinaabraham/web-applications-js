bcd56c4e3d6400fefe83f5ea30f813ce0a1afc8f
Christina Abraham <40750751+christinaabraham@users.noreply.github.com>
2019-04-13 22:56:52 -0500

    Update Ciphers.js

--- Ciphers.js
+++ Ciphers.js
@@ -1,43 +1,57 @@
-var text = returnText();
-var shifts = returnShifts();
-var cipher = completeCipher(text, shifts);
+//var text = returnText("enter_txt_input");
+//var shifts = returnShifts();
+//var cipher = completeCipher(text, shifts);
+
+var alphabet = "abcdefghijklmnopqrstuvwxyz ";
+var inputCaesarEnc = "";
+var inputCaesarDec = "";
+var shiftsCaesarEnc = 0;
+
 
 
 function chooseTool() {
   
 }
 
+onEvent("switch", "click", function() {
+  setScreen("caesar_dec_screen");
+});
 
-function returnText() {
-  var text = getText("enter_txt_input");
-  return text;
-}
+onEvent("enter_txt_input", "change", function(event) {
+ inputCaesarEnc = getText("enter_txt_input") 
+ console.log("Caesar enc input: " + inputCaesarEnc);
+});
 
-function returnShifts() {
-  var shifts = getNumber("num_shifts_input");
-  if(shifts > 26) {
-    shifts = shifts % 26;
-  }
-  return shifts;
-}
-
-
-onEvent("caesar_enc_btn", "click", function() {
-  console.log(text);
-  console.log(shifts);
-  console.log(cipher);
-  setScreen("caesar_enc_result_screen");
-  setText("caesar_enc_text_area", cipher);
+onEvent("text_input1", "change", function(event) {
+ inputCaesarDec = getText("text_input1") 
+ console.log("Caesar dec input: " + inputCaesarDec);
 });
 
 
-function completeCipher(text, shifts) {
-  var alphabet = "abcdefghijklmnopqrstuvwxyz";
+onEvent("num_shifts_input", "change", function(event) {
+  temp = getNumber("num_shifts_input");
+  if(temp > 26) {
+    temp = temp % 26;
+  }
+  shiftsCaesarEnc = temp;
+  console.log("Caesar enc shifts: " + shiftsCaesarEnc);
+});
+
+
+onEvent("caesar_enc_btn", "click", function() {
+  encryptCaesar(inputCaesarEnc, shiftsCaesarEnc);
+});
+
+onEvent("button1", "click", function () {
+  decryptCaesar(inputCaesarDec);
+});
+
+function encryptCaesar(text, shifts) {
   var cipher = "";
   var index, location = 0;
         
   for(var k = 0; k < text.length; k++) {
-    if(text.substring(k, k+1).equals(" ")) {
+    if(text.substring(k, k+1) == " ") {
         cipher += " ";
         k++;
     }
@@ -51,6 +65,50 @@ function completeCipher(text, shifts) {
       cipher += alphabet.substring(location, location + 1);
     }  
   }
+  setText("caesar_enc_text_area", cipher);
+  setScreen("caesar_enc_result_screen");
 } 
 
+function decryptCaesar(text) {
+  console.log(text);
+  var shifts, location, index = 0;
+  var input = text;
+  var output = "";
 
+          for(var j = 0; j < 26; j++) {
+            shifts = j;
+            
+            if(shifts < 9) {
+                output += " " + (shifts + 1) + ". ";
+            } else {
+                output += (shifts + 1) + ". ";
+            }
+
+            for(var i = 0; i < input.length; i++) {
+                if(input.substring(i, i+1) == " ") {
+                    output += " ";
+                    i++;
+                }
+                
+                index = alphabet.indexOf(input.toLowerCase().substring(i, i+1));
+                location = index + shifts;
+
+                if(location > 25) {
+                    output += alphabet.substring(location - 26, location - 25);
+                } else {
+                    output += alphabet.substring(location, location + 1);
+                }
+            }
+            output += "\n";
+        }
+  //setText("caesar_enc_text_area", cipher);
+  //setScreen("caesar_enc_result_screen");
+  setText("text_area1", output);
+  setScreen("screen2");
+  console.log(output);
+}
+
+
+onEvent("label5", "click", function(event) {
+  console.log("label5 clicked!");
+});
