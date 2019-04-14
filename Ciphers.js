1208c497ca9a6ad132a68b224412b47ea31fcfee
Christina Abraham <40750751+christinaabraham@users.noreply.github.com>
2019-04-14 00:55:44 -0500

    Update Ciphers.js

--- Ciphers.js
+++ Ciphers.js
@@ -2,10 +2,13 @@
 //var shifts = returnShifts();
 //var cipher = completeCipher(text, shifts);
 
+var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphabet = "abcdefghijklmnopqrstuvwxyz ";
 var inputCaesarEnc = "";
 var inputCaesarDec = "";
 var shiftsCaesarEnc = 0;
+
+//console.log(vigenereEncrypt("testing one two three"));
 
 
 
@@ -75,32 +78,32 @@ function decryptCaesar(text) {
   var input = text;
   var output = "";
 
-          for(var j = 0; j < 26; j++) {
-            shifts = j;
-            
-            if(shifts < 9) {
-                output += " " + (shifts + 1) + ". ";
-            } else {
-                output += (shifts + 1) + ". ";
-            }
+  for(var j = 0; j < 26; j++) {
+    shifts = j;
+    
+    if(shifts < 9) {
+        output += " " + (shifts + 1) + ". ";
+    } else {
+        output += (shifts + 1) + ". ";
+    }
 
-            for(var i = 0; i < input.length; i++) {
-                if(input.substring(i, i+1) == " ") {
-                    output += " ";
-                    i++;
-                }
-                
-                index = alphabet.indexOf(input.toLowerCase().substring(i, i+1));
-                location = index + shifts;
-
-                if(location > 25) {
-                    output += alphabet.substring(location - 26, location - 25);
-                } else {
-                    output += alphabet.substring(location, location + 1);
-                }
-            }
-            output += "\n";
+    for(var i = 0; i < input.length; i++) {
+        if(input.substring(i, i+1) == " ") {
+            output += " ";
+            i++;
         }
+        
+        index = alphabet.indexOf(input.toLowerCase().substring(i, i+1));
+        location = index + shifts;
+
+        if(location > 25) {
+            output += alphabet.substring(location - 26, location - 25);
+        } else {
+            output += alphabet.substring(location, location + 1);
+        }
+    }
+    output += "\n";
+}
   //setText("caesar_enc_text_area", cipher);
   //setScreen("caesar_enc_result_screen");
   setText("text_area1", output);
@@ -112,3 +115,86 @@ function decryptCaesar(text) {
 onEvent("label5", "click", function(event) {
   console.log("label5 clicked!");
 });
+
+function vigenereEncrypt(text){
+  var index = 0;
+  var count = 0;
+  var check = "";
+  var output = "";
+  var old = "";
+  var anew = "";
+  var verify = "";
+  var outputArr = [];
+
+  for(var j = 0; j < text.length; j++) {
+    appendItem(outputArr, text.substring(j, j+1).toLowerCase());
+  }
+
+  for(var i = 0; i < text.length; i++) {
+    index = randomNumber(0, 25 - count);
+    
+    while(check.includes(alpha[index])){ //
+      index = randomNumber(0, 25 - count);
+    }
+    
+    anew = alpha[index];
+    console.log("Alpha length: " + alpha.length);
+    console.log("Index: " + index);
+
+    old = text.substring(i, i+1);
+    check += anew;
+    //console.log(check);
+
+    for(var k = 0; k < text.length; k++) {
+      if(outputArr[k] == old && !verify.includes(k)) {
+        outputArr[k] = anew;
+        verify += k + "_";
+      }
+    }
+    removeItem(alpha, index);
+    count++;
+  }
+  
+  //console.log(alpha);
+  //console.log(outputArr);
+  //console.log("Check: " + check);
+  
+  for(var a = 0; a < text.length; a++) {
+    output += outputArr[a];
+  }
+  
+  //console.log(text);
+  return output;
+}
+
+decryptVigenere("Testing");
+
+function decryptVigenere(text) {
+  var old = "";
+  var count = 0;
+  var library = [];
+  var frequency = [];
+  var check = "";
+  
+  for(var a = 0; a < text.length; a++) {
+    if(check.includes(text.substring(a, a+1))) {
+      a++;
+    }
+    appendItem(library, text.substring(a, a+1));
+    check += text.substring(a, a+1);
+  }
+  
+  for(var i = 0; i < text.length; i++) {
+    old = text.substring(i, i+1);
+  
+    for(var j = 0; j < text.length; j++) {
+      if(text.substring(j, j+1) == old) {
+        count++;
+      } 
+    }
+  }
+  
+  console.log(library);
+  console.log(frequency);
+  console.log("Check: " + check);
+}
